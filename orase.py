5ff1d60dfb8ef84acb1e17b93834a621d5280e98
Adina <adinalambru663@gmail.com>
2026-05-16 16:26:54 +0300

    feat: adaug functionalitate Como - populatie si descriere

--- orase.py
+++ orase.py
@@ -1,1 +1,14 @@
 .
+from app.lib.biblioteca_orase import populatie_como, descriere_como
+
+@app.route('/como')
+def como():
+    return '<h2>Como</h2><a href="/como/populatie">Populatie</a> | <a href="/como/descriere">Descriere</a>'
+
+@app.route('/como/populatie')
+def como_populatie():
+    return populatie_como()
+
+@app.route('/como/descriere')
+def como_descriere():
+    return descriere_como()
