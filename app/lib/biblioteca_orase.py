f241563198796814701e1f3fa9f6cc9faea8a10c
Adina <adinalambru663@gmail.com>
2026-05-16 18:41:12 +0300

    fix: adaug app/lib in git tracking

---
+++ app/lib/biblioteca_orase.py
@@ -0,0 +1,5 @@
+def populatie_como():
+    return "Como are aproximativ 84.000 de locuitori."
+
+def descriere_como():
+    return "Como este un oras din nordul Italiei, asezat pe malul lacului Como, cunoscut pentru arhitectura eleganta si proximitatea fata de Alpi."
