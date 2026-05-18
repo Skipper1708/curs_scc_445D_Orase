eca41c25407482bb1f9d3c79c6a561362fa3c647
Skipper1708 <victorvls1995@gmail.com>
2026-05-18 20:24:56 +0300

    Add app/lib/biblioteca_orase.py

---
+++ app/lib/biblioteca_orase.py
@@ -0,0 +1,6 @@
+def populatie_barcelona():
+    return "Barcelona are o populatie de aproximativ 1.6 milioane de locuitori (2023)."
+
+
+def descriere_barcelona():
+    return "Barcelona este capitala Cataloniei, situata pe coasta mediteraneana a Spaniei, cunoscuta pentru arhitectura lui Gaudi si pentru FC Barcelona."
