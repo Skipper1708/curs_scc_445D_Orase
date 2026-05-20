ebb62743f2318340fbfd0092c341540c567d4b0a
Sirbu Rares <sirburares656@yahoo.com>
2026-05-20 18:56:58 +0300

    fix gitignore, add app/lib with biblioteca_orase

---
+++ app/lib/biblioteca_orase.py
@@ -0,0 +1,6 @@
+def populatie_roma():
+    return "Roma are o populatie de aproximativ 2.8 milioane de locuitori (2023)."
+
+
+def descriere_roma():
+    return "Roma este capitala Italiei, cunoscuta ca Orasul Etern, cu o istorie de peste 2700 de ani, fondata pe sapte coline, si gazda Vaticanului."
