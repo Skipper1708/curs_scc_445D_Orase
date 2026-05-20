8481b286abe8c3871eb88a70717565c9a09a0cb0
Mitrea Tudor-Andrei <tudormitrea@gmail.com>
2026-05-20 21:59:30 +0300

    add beautiful bootstrap interface and update readme

--- orase.py
+++ orase.py
@@ -1,24 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from app.lib.biblioteca_orase import populatie_manchester, descriere_manchester
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='app/templates') # Îi spunem exact unde e folderul
 
 @app.route('/')
 def home():
-    return '<h1>Aplicatie Orase - SCC 445D</h1><p><a href="/orase">Lista orase</a></p>'
+    descriere = descriere_manchester()
+    populatie = populatie_manchester()
+    return render_template('index.html', descriere=descriere, populatie=populatie)
 
-@app.route('/orase')
-def orase():
-    return '<h1>Orase disponibile</h1><ul><li><a href="/manchester">Manchester</a></li></ul>'
-
-@app.route('/manchester')
-def manchester():
-    return '<h1>Manchester</h1><ul><li><a href="/manchester/populatie">Populatie</a></li><li><a href="/manchester/descriere">Descriere</a></li></ul>'
-
-@app.route('/manchester/populatie')
-def manchester_populatie():
-    return populatie_manchester()
-
-@app.route('/manchester/descriere')
-def manchester_descriere():
-    return descriere_manchester()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5011)
