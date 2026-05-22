9b423e7f295b6a8834ce220a1008e016113cedd4
Ioana Delia <deliaelena4592@gmail.com>
2026-05-22 05:09:01 +0300

    add app/lib missing files

---
+++ app/lib/biblioteca_orase.py
@@ -0,0 +1,5 @@
+def populatie_varsovia():
+    return "Varsovia are o populatie de aproximativ 1.8 milioane de locuitori (2023)."
+
+def descriere_varsovia():
+    return "Varsovia este capitala Poloniei, situata pe raul Vistula, cunoscuta pentru Orasul Vechi reconstruit dupa cel de-al Doilea Razboi Mondial si pentru Palatul Culturii si Stiintei."
